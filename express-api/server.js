2f98f3597484e4d30662db3d2aeb8162e11e6d42
wjw12 <jiewenw96@gmail.com>
2020-08-31 23:27:50 +0000

    working

--- express-api/server.js
+++ express-api/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {exec} = require('child_process');
+const {exec, spawn} = require('child_process');
 
 const app = express();
 app.use(bodyParser.json());
@@ -17,16 +17,18 @@ app.post('/stream', (req, res) => {
   const name = req.body.name;
   const url = req.body.url;
   if (!name || !url) {res.status(400).send({"success": false}); return;}
-  if (streams[name]) {res.status(200).send(streams[name]); return;}
+  if (streams[name]) {res.status(200).send({"success": true}); return;}
 
-  // launch process
-  //exec('./libav-RTMP-Streaming/build/streamer', {env: {SRC: url, DST: 'rtmp://127.0.0.1/live/' + name }},
-  exec('cd /files && make build && docker run --name streamer -w /files --rm -t -v `pwd`:/files -e SRC=' + url + ' -e DST=rtmp://127.0.0.1/live/' + name + ' leandromoreira/ffmpeg-devel /files/build/streamer',
-  (err, stdout, stderr) => {
-    if (err) {console.log(`error: ${err.message}`); res.status(400).send({"success": false, "msg": err.message}); return;}
-    if (stderr) {console.log(`stderr: ${stderr}`); res.status(400).send({"success": false, "msg": stderr}); return;}
-    console.log(`stdout: ${stdout}`);
-  });
+  const docker = spawn('docker', ('run --network host --rm -e SRC='+url+' -e DST=rtmp://127.0.0.1/live/'+name+' streamer:latest').split(' '));
+  docker.stdout.on('data', (data) => { console.log(`stdout: ${data}`);});
+  docker.stderr.on('data', (data) => {console.log(`stderr: ${data}`);});
+  docker.on('close', (code) => {console.log(`child process exited with code ${code}`);});
+//exec('cd /files && make build && docker run --name streamer -w /files --rm -v `pwd`:/files -e SRC=' + url + ' -e DST=rtmp://127.0.0.1/live/' + name + ' leandromoreira/ffmpeg-devel /files/build/streamer',
+//  (err, stdout, stderr) => {
+//    if (err) {console.log(`error: ${err.message}`); res.status(400).send({"success": false, "msg": err.message}); return;}
+//    if (stderr) {console.log(`stderr: ${stderr}`); res.status(400).send({"success": false, "msg": stderr}); return;}
+//    console.log(`stdout: ${stdout}`);
+//  });
   
   streams[name] = {
     "name": name,
--- libav-RTMP-Streaming/src/main.cpp
+++ libav-RTMP-Streaming/src/main.cpp
@@ -4,13 +4,16 @@
 
 int main(int argc, char *argv[])
 {
+        const char* src = getenv("SRC");
+        const char* rtmp = getenv("RTMP");
+        const char* rtsp = getenv("RTSP");
+        if (!src || !rtmp || !rtsp) return 0;
 	std::cout << std::endl
 		<< "Welcome to RTMP streamer" << std::endl
 		<< "Written by Jiewen Wang <https://jiewen.wang>" << std::endl
 		<< std::endl;
         std::cout << "Now streaming from " << getenv("SRC") << std::endl;
-	//Streamer streamer("samples/test.avi", "rtmp://127.0.0.1/live");
-	Streamer streamer(getenv("SRC"), getenv("DST"));
+	Streamer streamer(src, rtmp, rtsp);
 
 	return streamer.Stream();
 }
